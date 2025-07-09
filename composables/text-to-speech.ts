dc31914357b717a8416effb731c60c383a5ab0b3
Denish Sharma <denishcommon@gmail.com>
2025-07-09 15:06:03 +0530

    fix: update WebSocket URL and payload structure for fetching audio

--- composables/text-to-speech.ts
+++ composables/text-to-speech.ts
@@ -1,10 +1,16 @@
 import { destr } from "destr";
 
-const { send, data: wsData } = useWebSocket("wss://asr.iitm.ac.in/demo/ws/tts/english/male");
+const { send, data: wsData } = useWebSocket("wss://asr.iitm.ac.in/demo/ws/tts/");
 
 export function useFetchWordAudio(word: string) {
+    const payload = {
+        text: word,
+        alpha: "1",
+        language: "english",
+        gender: "male",
+    };
     return new Promise<string>((resolve) => {
-        send(word);
+        send(JSON.stringify(payload));
         const unwatch = watch(wsData, (value) => {
             const { audio } = destr<{ audio: string }>(value);
             resolve(audio);
